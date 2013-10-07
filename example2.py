b41221eaeb7e2aecfdc96983f311b88bf9d94ae8
matias <matias@brian.(none)>
2013-10-07 21:19:58 +0200

    added example2, showing a known issue

--- example2.py
+++ example2.py
@@ -5,24 +5,17 @@ import gevent
 from gevent import monkey; monkey.patch_all()
 import functools
 import time
-
-def test_greenlet(name):
-  i = 1
-  while True:
-    print name, i
-    i += 1
-    time.sleep(1)
+import greenlet
 
 def btn_clicked():
-  gevent.spawn(test_greenlet, "C")
+  print 'before sleep'
+  gevent.sleep(3)
+  print 'after sleep'
 
 if __name__ == '__main__':
   app = QtGui.QApplication([])
   mainwin = QtGui.QMainWindow()
   btn = QtGui.QPushButton('Start greenlet', mainwin) 
   btn.clicked.connect(btn_clicked)
-  gevent.spawn(test_greenlet, 'A')
-  gevent.spawn(test_greenlet, 'B')
-  
   mainwin.show()
   app.exec_()
